538bfba7dfc7a8169e3ca92055d30861bc486425
Akhil Jacob <akhilcjacob@gmail.com>
2017-11-29 21:44:12 +0000

    Added temp homepage, created production homepage, added link component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LinksComponent } from './links/links.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LinksComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/links/links.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Link } from '../link';
+
+@Component({
+  selector: 'app-links',
+  templateUrl: './links.component.html',
+  styleUrls: ['./links.component.css']
+})
+export class LinksComponent implements OnInit {
+  link: Link = {
+	id: 'http://resume.akhilcjacob.com';
+	name: 'resume';
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
